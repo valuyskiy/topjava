52728cad8b628176e6bc1cf1d75a97d00e7a5c6e
Alexandr Valuyskiy <alexandr@valuyskiy.ru>
2018-03-12 23:02:33 +0300

    Realize layers of app

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,35 @@
 package ru.javawebinar.topjava.web.meal;
 
-import ru.javawebinar.topjava.service.MealService;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 
-public class MealRestController {
-    private MealService service;
+import java.util.List;
 
+@Controller
+public class MealRestController extends AbstractMealController{
+
+    @Override
+    public Meal get(int id) {
+        return super.get(id);
+    }
+
+    @Override
+    public Meal create(Meal meal) {
+        return super.create(meal);
+    }
+
+    @Override
+    public void delete(int id) {
+        super.delete(id);
+    }
+
+    @Override
+    public void update(Meal meal, int id) {
+        super.update(meal, id);
+    }
+
+    @Override
+    public List<Meal> getAll() {
+        return super.getAll();
+    }
 }
