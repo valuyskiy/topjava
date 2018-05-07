bb0951f2b41f5a43c3d9a681a09803a2955acada
Alexandr Valuyskiy <alexandr@valuyskiy.ru>
2018-05-07 12:34:55 +0300

    Fix meal tests

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,8 +1,6 @@
 package ru.javawebinar.topjava.web;
 
 import org.junit.Test;
-import ru.javawebinar.topjava.MealTestData;
-import ru.javawebinar.topjava.util.MealsUtil;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -36,9 +34,16 @@ public class RootControllerTest extends AbstractControllerTest {
         mockMvc.perform(get("/meals")
                 .with(userAuth(USER)))
                 .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(view().name("meals"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-                .andExpect(model().attribute("meals", MealsUtil.getWithExceeded(MealTestData.MEALS, USER.getCaloriesPerDay())));
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"));
     }
 
+    @Test
+    public void testMealsUnAuth() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
 }
