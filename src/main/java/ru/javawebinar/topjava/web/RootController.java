e45978555927d59d3eb8d761d87e054b3d0807c7
Alexandr Valuyskiy <alexandr@valuyskiy.ru>
2018-05-15 23:53:03 +0300

    Add error handling for duplicate email

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -1,21 +1,35 @@
 package ru.javawebinar.topjava.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.support.SessionStatus;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.to.UserTo;
 import ru.javawebinar.topjava.util.UserUtil;
+import ru.javawebinar.topjava.web.Validator.ProfileFormValidator;
 import ru.javawebinar.topjava.web.user.AbstractUserController;
 
 import javax.validation.Valid;
 
 @Controller
 public class RootController extends AbstractUserController {
+
+    @Autowired
+    ProfileFormValidator profileValidator;
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.addValidators(profileValidator);
+    }
+
 
     @GetMapping("/")
     public String root() {
@@ -45,7 +59,7 @@ public class RootController extends AbstractUserController {
     }
 
     @PostMapping("/profile")
-    public String updateProfile(@Valid UserTo userTo, BindingResult result, SessionStatus status) {
+    public String updateProfile(@Valid @Validated UserTo userTo, BindingResult result, SessionStatus status) {
         if (result.hasErrors()) {
             return "profile";
         } else {
---
+++ src/main/java/ru/javawebinar/topjava/web/Validator/ProfileFormValidator.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.topjava.web.Validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+@Component
+public class ProfileFormValidator implements Validator {
+
+    @Autowired
+    protected UserService service;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return UserTo.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        try {
+            UserTo userTo = (UserTo) target;
+            User updatedUser = service.getByEmail(userTo.getEmail());
+            if (AuthorizedUser.id() != updatedUser.getId()) {
+                errors.rejectValue("email", "user.emailExist");
+            }
+        } catch (NotFoundException e) {
+            // ой, не хорошо так делать
+        }
+    }
+}
